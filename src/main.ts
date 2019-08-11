2f5439dec70529ab05c4d11554a397e8baa787c0
Pavol Hejny <me@pavolhejny.com>
2019-08-11 12:25:34 +0200

    Prettier

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { forImmediate } from './functions/forImmediate';
 import { forTime } from './functions/forTime';
 import { forValueDefined } from './functions/forValueDefined';
 
-export { forAllImagesInElement, forAnimationFrame, forImmediate, forTime, forValueDefined, forDocumentLoad };
+export {
+    forAllImagesInElement,
+    forAnimationFrame,
+    forImmediate,
+    forTime,
+    forValueDefined,
+    forDocumentLoad,
+};
 
 // TODO: forever
