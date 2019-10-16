c05db73093a2755b92b77ca3489fdc3eeefa051d
Pavol Hejny <me@pavolhejny.com>
2019-10-16 15:05:50 +0200

    Prettier

--- src/functions/forAllImagesInElement.ts
+++ src/functions/forAllImagesInElement.ts
@@ -2,28 +2,26 @@ export async function forAllImagesInElement(
     element: HTMLElement,
 ): Promise<void> {
     await Promise.all(
-        Array.from(element.querySelectorAll('img')).map(
-            (imgElement) => {
-                return new Promise((resolve, reject) => {
-                    if(imgElement.complete) {
-                        if(imgElement.naturalHeight === 0) {
-                            reject(imgElement);
-                        } else {
-                            resolve();
-                        }
-
-                        return;
-                    };
-
-                    imgElement.addEventListener('load', () => {
-                        resolve();
-                    });
-                    imgElement.addEventListener('error', () => {
+        Array.from(element.querySelectorAll('img')).map((imgElement) => {
+            return new Promise((resolve, reject) => {
+                if (imgElement.complete) {
+                    if (imgElement.naturalHeight === 0) {
                         reject(imgElement);
-                    });
-                })
-            }
-        ),
+                    } else {
+                        resolve();
+                    }
+
+                    return;
+                }
+
+                imgElement.addEventListener('load', () => {
+                    resolve();
+                });
+                imgElement.addEventListener('error', () => {
+                    reject(imgElement);
+                });
+            });
+        }),
     );
     return;
 }
