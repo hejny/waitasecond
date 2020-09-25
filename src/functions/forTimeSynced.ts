01eae045ada079bca10836abc6090cc28f4160af
Pavol Hejny <me@pavolhejny.com>
2020-09-25 13:01:00 +0200

    forTimeSynced

---
+++ src/functions/forTimeSynced.ts
@@ -0,0 +1,12 @@
+import { forTime } from "./forTime";
+
+/**
+ * This waiter function will sync some action to some precise time
+ * @param period looping period in milliseconds
+ * @param shift according to current time in milliseconds
+ */
+export async function forTimeSynced(period: number, shift: number = 0): Promise<void> {
+    const currentTime = new Date().getTime();
+    const wait = period - ((currentTime - shift) % period);
+    await forTime(wait);
+}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { forAnimationFrame } from './functions/forAnimationFrame';
 import { forDocumentLoad } from './functions/forDocumentLoad';
 import { forImmediate } from './functions/forImmediate';
 import { forTime } from './functions/forTime';
+import { forTimeSynced } from './functions/forTimeSynced';
 import { forValueDefined } from './functions/forValueDefined';
 
 export {
@@ -10,8 +11,11 @@ export {
     forAnimationFrame,
     forImmediate,
     forTime,
+    forTimeSynced,
     forValueDefined,
     forDocumentLoad,
 };
 
+// TODO: Auto add
+// TODO: Auto update where it is used (but in more clever way)
 // TODO: forever
