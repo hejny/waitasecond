6ec5a7eb941033eb1ed7680ddb3328a36d52c58b
Pavol Hejny <me@pavolhejny.com>
2020-09-25 13:02:06 +0200

    Prettier

--- src/functions/forTimeSynced.ts
+++ src/functions/forTimeSynced.ts
@@ -1,11 +1,14 @@
-import { forTime } from "./forTime";
+import { forTime } from './forTime';
 
 /**
  * This waiter function will sync some action to some precise time
  * @param period looping period in milliseconds
  * @param shift according to current time in milliseconds
  */
-export async function forTimeSynced(period: number, shift: number = 0): Promise<void> {
+export async function forTimeSynced(
+    period: number,
+    shift: number = 0,
+): Promise<void> {
     const currentTime = new Date().getTime();
     const wait = period - ((currentTime - shift) % period);
     await forTime(wait);
