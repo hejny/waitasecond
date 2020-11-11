fd10d2191688ea72090fdde98d530e4c4a21969e
Pavol Hejny <me@pavolhejny.com>
2020-11-12 00:03:55 +0100

    Waiting forTimeout

---
+++ src/functions/forTimeout.ts
@@ -0,0 +1,35 @@
+import { forTime } from "./forTime";
+
+export async function forTimeout<T>(
+    timeout: number,
+    value: Promise<T>,
+): Promise<T> {
+    return await Promise.race([
+        value,
+        (async () => {
+            await forTime(timeout);
+            throw new Error(`Timeout ${timeout}ms is up.`);
+        })(),
+    ]);
+}
+
+/*
+Note: Possible alternative way:
+
+interface IForTimeoutOptions<T> {
+    timeout: number;
+    value: Promise<T>;
+}
+
+async function forTimeout<T>(
+    options: IForTimeoutOptions<T>,
+): Promise<T> {
+    return await Promise.race([
+        options.value,
+        (async () => {
+            await forTime(options.timeout);
+            throw new Error(`Timeout ${options.timeout}ms is up.`);
+        })(),
+    ]);
+}
+*/
