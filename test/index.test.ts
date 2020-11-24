9dfffd007354f73530f8f6af25a3b9d77cf87898
Pavol Hejny <me@pavolhejny.com>
2020-11-24 15:22:33 +0100

    Actually I do not know what is the best way how to test library of this type

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,7 @@
 import { forTime } from '../src/main';
 
+// TODO: Actually I do not know what is the best way how to test library of this type
+
 test('WaitASecond', () => {
     expect(async () => {
         await forTime(10);
