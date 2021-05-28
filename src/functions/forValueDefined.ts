93a403c689aa052b0670267fa66d9bc72c3539d3
Pavol Hejny <me@pavolhejny.com>
2021-05-29 00:16:06 +0200

    forValueDefined can have awaitable value

--- src/functions/forValueDefined.ts
+++ src/functions/forValueDefined.ts
@@ -1,15 +1,17 @@
 import { forAnimationFrame } from './forAnimationFrame';
 
+type IAwaitable<T> = T | Promise<T>;
+
 export async function forValueDefined<T>(
-    getValue: () => T | undefined,
+    getValue: () => IAwaitable<T | undefined | null>,
     waiter: () => Promise<void> = async () => {
         await forAnimationFrame();
     },
     limit: number = 1000,
 ): Promise<T> {
     for (let i = 0; i < limit; i++) {
-        const value = getValue();
-        if (typeof value !== 'undefined') {
+        const value = await getValue();
+        if (value !== undefined && value !== null) {
             return value;
         }
         await waiter();
