32208ad99378f63309c39c86426167b52039656d
Pavol Hejny <me@pavolhejny.com>
2021-09-11 23:06:00 +0200

    Throttling in samples server

---
+++ samples/samplesServer/samplesServer.ts
@@ -0,0 +1,34 @@
+// TODO: !!! Import
+import { forTime } from '../../src/functions/forTime';
+import express from 'express';
+import serveStatic from 'serve-static';
+import serveIndex from 'serve-index';
+import path from 'path';
+// import opn from 'open';
+
+// TODO: !!! Uninstall express-throttle-bandwidth
+// const throttle = require('express-throttle-bandwidth');
+
+// TODO: as indipendent library
+
+const PORT = 8070;
+
+const staticBasePath = path.join(__dirname, '../../');
+
+const app = express();
+
+//app.use(throttle(10 /* bits per second */));
+
+app.use(async (request, response, next) => {
+    request;
+    response;
+    await forTime(3333);
+    next();
+});
+app.use(serveStatic(staticBasePath, { index: false, cacheControl: false }));
+app.use(serveIndex(staticBasePath, { icons: true }));
+app.listen(PORT);
+
+console.log(`Static server listening on port ${PORT}.`);
+// TODO: Only if not opened
+// !!! opn(`http://localhost:${PORT}/samples`);
