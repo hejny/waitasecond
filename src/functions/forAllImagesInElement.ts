5d2b4cc44c7ad98efbb7d808c05a87555bc4e99a
Pavol Hejny <me@pavolhejny.com>
2022-02-11 11:50:55 +0100

    Fix lint

--- src/functions/forAllImagesInElement.ts
+++ src/functions/forAllImagesInElement.ts
@@ -13,39 +13,41 @@ TODO: Verbose mode for console.info
 
 export function forAllImagesInElement(element: HTMLElement): Promise<void> {
     return Promise.all<void>(
-        Array.from(element.querySelectorAll('img')).map((imgElement, i) => {
-            return new Promise((resolve, reject) => {
-                if (imgElement.complete) {
-                    if (imgElement.naturalHeight === 0) {
-                        /*
+        Array.from(element.querySelectorAll('img')).map(
+            (imgElement /*, i*/) => {
+                return new Promise((resolve, reject) => {
+                    if (imgElement.complete) {
+                        if (imgElement.naturalHeight === 0) {
+                            /*
                         console.info(
                             `Image ${i} rejected due to 0 naturalHeight`,
                         );
                         */
 
-                        // TODO: !!! Check this also in load
-                        reject(imgElement);
-                    } else {
-                        // console.info(`Image ${i} is already completed`);
-                        resolve();
+                            // TODO: !!! Check this also in load
+                            reject(imgElement);
+                        } else {
+                            // console.info(`Image ${i} is already completed`);
+                            resolve();
+                        }
                     }
-                }
 
-                imgElement.addEventListener('load', () => {
-                    // console.info(`Image ${i} resolved`);
-                    resolve();
+                    imgElement.addEventListener('load', () => {
+                        // console.info(`Image ${i} resolved`);
+                        resolve();
+                    });
+                    imgElement.addEventListener('error', () => {
+                        // console.info(`Image ${i} rejected`);
+                        // TODO: !!! imgElement into ImageError
+                        reject(
+                            new Error(
+                                `Some images can\`t be loaded. If you want to supress this error keep rejectWhenNotLoaded=false (default value).`,
+                            ),
+                        );
+                    });
                 });
-                imgElement.addEventListener('error', () => {
-                    // console.info(`Image ${i} rejected`);
-                    // TODO: !!! imgElement into ImageError
-                    reject(
-                        new Error(
-                            `Some images can\`t be loaded. If you want to supress this error keep rejectWhenNotLoaded=false (default value).`,
-                        ),
-                    );
-                });
-            });
-        }),
+            },
+        ),
     ).then(() => {
         /* Note: Returning void */
     });
