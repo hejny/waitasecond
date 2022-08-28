16cdd8895df44fdd64dfda0541096b6b4c19b73c
Pavol Hejny <me@pavolhejny.com>
2022-08-29 01:58:57 +0200

    Fix issue with interactive readyState

--- src/functions/forDocumentReady.ts
+++ src/functions/forDocumentReady.ts
@@ -3,15 +3,21 @@
  * Creates a promise that resolves when the document.readyState is on targetReadyState
  */
 export function forDocumentReady(
-    targetReadyState: 'interactive' | 'complete' = 'interactive',
+    targetReadyState: 'interactive' | 'complete' = 'complete',
 ): Promise<void> {
-    if (document.readyState === targetReadyState) {
+    if (
+        document.readyState === targetReadyState ||
+        document.readyState === 'complete'
+    ) {
         return Promise.resolve();
     }
 
     return new Promise((resolve) => {
         window.addEventListener('readystatechange', () => {
-            if (document.readyState === targetReadyState) {
+            if (
+                document.readyState === targetReadyState ||
+                document.readyState === 'complete'
+            ) {
                 resolve();
             }
         });
