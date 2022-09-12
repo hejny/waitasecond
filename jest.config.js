78618285f4279152b12b8818743794a657449459
Restyled.io <commits@restyled.io>
2022-09-12 09:19:14 +0000

    Restyled by prettier

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  transform : {
-    '(jsx?|tsx?)$' : 'ts-jest',
-  },
-  testRegex : '(test)\\.(jsx?|tsx?)$',
-  moduleFileExtensions : [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
-  coverageDirectory : './coverage/',
-  collectCoverage : true,
+    transform: {
+        '(jsx?|tsx?)$': 'ts-jest',
+    },
+    testRegex: '(test)\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    coverageDirectory: './coverage/',
+    collectCoverage: true,
 };
